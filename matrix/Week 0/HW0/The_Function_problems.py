bb96d35b3f285466395f8997ec62d4bae2a196a6
eneskemalergin <eneskemalergin@gmail.com>
2015-02-21 21:40:16 -0600

    Completed The_Function_problems now error-free and successfully submitted.

--- matrix/Week 0/HW0/The_Function_problems.py
+++ matrix/Week 0/HW0/The_Function_problems.py
@@ -20,7 +20,11 @@ def tuple_sum(A, B):
     >>> tuple_sum([(1,2), (10,20)],[(3,4), (30,40)])
     [(4, 6), (40, 60)]
     '''
-    pass
+    result = []
+    for i in range(len(A)):
+        tup = (A[i][0] + B[i][0], A[i][1] + B[i][1])
+        result.append(tup)
+    return result
 
 
 
@@ -35,37 +39,33 @@ def inv_dict(d):
     Example:
     >>> inv_dict({'goodbye':  'au revoir', 'thank you': 'merci'}) == {'merci':'thank you', 'au revoir':'goodbye'}
     '''
-    pass
+
+    result = {}
+
+    for i in d.keys():
+        result[d[i]] = i
+        
+    return result
 
 
 
 ## 3: (Problem 0.8.5) Nested Comprehension
-def row(p, n):
-    '''
-    Input:
-      -p: a number
-      -n: a number
-    Output:
-      - n-element list such that element i is p+i
-    Examples:
-    >>> row(10,4)
-    [10, 11, 12, 13]
-    '''
-    pass
+def row(p, n): return [x for x in range(p, p+n)]
 
-comprehension_with_row = ...
 
-comprehension_without_row = ...
+comprehension_with_row = [row(i, 20) for i in range(15)]
+
+comprehension_without_row = [[i+j for j in range(20)]for i in range(15)]
 
 
 
 ## 4: (Problem 0.8.10) Probability Exercise 1
-Pr_f_is_even = ...
-Pr_f_is_odd  = ...
+Pr_f_is_even = 0.7
+Pr_f_is_odd  = 0.3
 
 
 
 ## 5: (Problem 0.8.11) Probability Exercise 2
-Pr_g_is_1    = ...
-Pr_g_is_0or2 = ...
+Pr_g_is_1    = 0.4
+Pr_g_is_0or2 = 0.6
 
