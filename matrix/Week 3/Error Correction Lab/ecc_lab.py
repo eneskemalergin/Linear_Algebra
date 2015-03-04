cebece022850f7895c245bf5660e449d28f31bb6
eneskemalergin <eneskemalergin@gmail.com>
2015-03-04 09:55:31 -0600

    Week 3 homeworks and labs are completed, there are some parts missing or wrong

--- matrix/Week 3/Error Correction Lab/ecc_lab.py
+++ matrix/Week 3/Error Correction Lab/ecc_lab.py
@@ -5,6 +5,7 @@ coursera = 1
 from vec import Vec
 from mat import Mat
 from bitutil import bits2mat, str2bits, noise
+from matutil import *
 from GF2 import one
 
 ## Task 1
@@ -12,20 +13,19 @@ from GF2 import one
 the procedure listlist2mat in the matutil module (be sure to import first).
 Since we are working over GF (2), you should use the value one from the
 GF2 module to represent 1"""
-G = None
+G =  G = listlist2mat([[one,0,one,one],[one,one,0,one],[0,0,0,one],[one,one,one,0],[0,0,one,0],[0,one,0,0],[one,0,0,0]])
 
 ## Task 2
 # Please write your answer as a list. Use one from GF2 and 0 as the elements.
-encoding_1001 = None
+encoding_1001 = [0,0,one,one,0,0,one]
 
 
 ## Task 3
 # Express your answer as an instance of the Mat class.
-R = None
-
+R = listlist2mat([[0,0,0,0,0,0,one],[0,0,0,0,0,one,0],[0,0,0,0,one,0,0],[0,0,one,0,0,0,0]])
 ## Task 4
 # Create an instance of Mat representing the check matrix H.
-H = None
+H = listlist2mat([[0,0,0,one,one,one,one],[0,one,one,0,0,one,one],[one,0,one,0,one,0,one]])
 
 ## Task 5
 def find_error(syndrome):
@@ -42,14 +42,18 @@ def find_error(syndrome):
         >>> find_error(Vec({0,1,2}, {})) == Vec({0,1,2,3,4,5,6}, {})
         True
     """
-    pass
+    pos = sum([2**(2-i) for i in syndrome.f.keys() if syndrome.f[i] == one]) - 1
+    if pos < 0:
+        return Vec({0,1,2,3,4,5,6},{})
+    else:
+        return Vec({0,1,2,3,4,5,6},{pos:one})
 
 ## Task 6
 # Use the Vec class for your answers.
 non_codeword = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one, 6:one})
-error_vector = Vec(..., ...)
-code_word = Vec(..., ...)
-original = ... # R * code_word
+error_vector = Vec({0,1,2,3,4,5,6}, {6:one})
+code_word = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one})
+original = Vec({0,1,2,3}, {1:one,3:one}) #R * code_word
 
 
 ## Task 7
@@ -62,20 +66,17 @@ def find_error_matrix(S):
         >>> find_error_matrix(S) == Mat(({0, 1, 2, 3, 4, 5, 6}, {0, 1, 2, 3}), {(1, 3): 0, (3, 0): 0, (2, 1): 0, (6, 2): 0, (5, 1): one, (0, 3): 0, (4, 0): 0, (1, 2): 0, (3, 3): 0, (6, 3): 0, (5, 0): 0, (2, 2): 0, (4, 1): 0, (1, 1): 0, (3, 2): one, (0, 0): 0, (6, 0): 0, (2, 3): 0, (4, 2): 0, (1, 0): 0, (5, 3): 0, (0, 1): 0, (6, 1): 0, (3, 1): 0, (2, 0): 0, (4, 3): one, (5, 2): 0, (0, 2): 0})
         True
     """
-    pass
+    return coldict2mat({v:find_error(mat2coldict(S)[v]) for v in mat2coldict(S).keys()})
 
 ## Task 8
-s = "I'm trying to free your mind, Neo. But I can only show you the door. You're the one that has to walk through it."
-P = None
+s = "I'm trying to free your mind, Neo. But I can only show you the door. You’re the one that has to walk through it."
+
 
 ## Task 9
-C = None
-bits_before = None
-bits_after = None
 
 
-## Ungraded Task
-CTILDE = None
+
+
 
 ## Task 10
 def correct(A):
@@ -87,5 +88,5 @@ def correct(A):
         >>> correct(A) == Mat(({0, 1, 2, 3, 4, 5, 6}, {1, 2, 3}), {(0, 1): 0, (1, 2): 0, (3, 2): 0, (1, 3): 0, (3, 3): 0, (5, 2): one, (6, 1): 0, (3, 1): 0, (2, 1): 0, (0, 2): one, (6, 3): one, (4, 2): 0, (6, 2): one, (2, 3): 0, (4, 3): 0, (2, 2): 0, (5, 1): 0, (0, 3): one, (4, 1): 0, (1, 1): 0, (5, 3): one})
         True
     """
-    pass
+
 
